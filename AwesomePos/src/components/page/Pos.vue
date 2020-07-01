dd988fa354cb1374c18ff16e2c9552ff06d4dd0d
Liujun <liujun2j@126.com>
2020-07-01 12:50:43 +0800

    Vue-Pos

--- AwesomePos/src/components/page/Pos.vue
+++ AwesomePos/src/components/page/Pos.vue
@@ -1,17 +1,33 @@
 <template>
   <div class="pos">
+    <el-row>
+      <el-col :span='7' class="pos-order" id="order-list">
+        <el-tabs>
+
+        </el-tabs>
+      </el-col>
+    </el-row>
     
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'pos',
-  
+  mounted:function(){
+    //设置order-list高度
+    var orderHeight = document.body.clientHeight;
+    console.log(orderHeight);
+    document.getElementById("order-list").style.height = orderHeight+'px';
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.pos-order {
+  background-color: #f9fafc;
 
+}
 </style>
