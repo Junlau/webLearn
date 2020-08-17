1ab55765231a42a3574e18d1e6e4a3f73542eb00
Liujun <liujun2j@126.com>
2020-08-17 17:50:27 +0800

    筛选框

---
+++ 筛选框/sxk.js
@@ -0,0 +1,7 @@
+var personArr = [
+    {name:'王港', src:'./img/1.jpg',des:'哇哦杀菌灯',sex:'m'},
+    {name:'刘莹', src:'./img/1.jpg',des:'哇哦杀菌灯',sex:'f'},
+    {name:'王秀莹', src:'./img/1.jpg',des:'哇哦杀菌灯',sex:'f'},
+    {name:'刘金雷', src:'./img/1.jpg',des:'哇哦杀菌灯',sex:'m'},
+    {name:'刘飞翔', src:'./img/1.jpg',des:'哇哦杀菌灯',sex:'m'}
+];
