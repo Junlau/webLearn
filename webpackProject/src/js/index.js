1a8352bd04527abca3fc33611a403fcc5da62243
刘军(19054909) <19054909@suning.com>
2021-01-21 17:12:10 +0800

    增加数据请求和跳转

--- webpackProject/src/js/index.js
+++ webpackProject/src/js/index.js
@@ -1,2 +1,29 @@
+const { get } = require('jquery');
+
 require('../css/index.less');
-console.log('111111');
+require('jquery');
+
+function getGoodsList() {
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8080/api/goodsList.json',
+        success: function (data) {
+            createList(data);
+        }
+    })
+}
+getGoodsList();
+function createList(data) {
+    console.log(data);
+    let str = '';
+    data.list.forEach(function(ele, index){
+        //a标签跳转
+        str = str +`<a href="http://localhost:8080/goodsInfo.html?id=`+ele.id+`">
+        <div class="goodsItem">
+        <img src="`+ele.imgurl[0]+`" alt="">
+        <p class="goodsTitle">`+ele.name+`</p>
+        <p class="goodsPrice">￥`+ele.spectList[0].price+`</p>
+        </div></a>`
+    });
+    $('.table_content').html(str);
+}
