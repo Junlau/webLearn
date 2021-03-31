a95061d538fdb5418c26f2aefd15644a5c33d21c
liujun <liujun2j@126.com>
2021-03-31 17:57:19 +0800

    vue-music

--- vue-music/lj-music/src/App.vue
+++ vue-music/lj-music/src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <nice-header></nice-header>
     <router-view/>
+    <player-bar></player-bar>
+    <nice-footer></nice-footer>
+    <back-top></back-top>
   </div>
 </template>
 
@@ -12,12 +15,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/styles/iconfont.css';
 </style>
