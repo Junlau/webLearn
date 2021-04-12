cf90e2ab2594843f219b9f3dd192a3b5ac0c68eb
liujun <liujun2j@126.com>
2021-04-12 18:00:25 +0800

    增加推荐歌曲组件

---
+++ vue-music/lj-music/src/components/home/recommend-music/Index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="recommend-music">
+        <h2 class="title">推荐歌曲</h2>
+        <song-list :songList="songs"></song-list>
+    </div>
+</template>
+
+<script>
+import songList from '@/components/common/songList/Index'
+
+export default {
+    data () {
+       return {
+           songs: []
+       }
+    },
+    components: {
+        songList
+    }
+}
+</script>
+
+<style>
+
+</style>
