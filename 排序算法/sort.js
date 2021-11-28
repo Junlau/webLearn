27950f6ac03f789871e6b1152a06df3e46783eb1
Junlau <liujun2j@126.com>
2021-11-28 22:28:49 +0800

    LeetCode

--- 排序算法/sort.js
+++ 排序算法/sort.js
@@ -169,3 +169,171 @@ function pivotIndex(begin, end) {
 
 quickSort(creatArray)
 console.log(creatArray)
+
+
+
+//二叉搜索树删除节点
+//  Definition for a binary tree node.
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+let node = new TreeNode(50)
+node.left = new TreeNode(30)
+node.right = new TreeNode(70)
+node.left.right = new TreeNode(40)
+node.right.left = new TreeNode(60)
+node.right.right = new TreeNode(80)
+/**
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+ var deleteNode = function(root, key) {
+    if (!root) return root
+
+    let parentNode = null //父节点，因为删除需要父节点指向子节点
+    let p = root
+    let isright = false
+    //先找到
+    while (p !== null) {
+        if (p.val === key) {
+            break
+        }
+        parentNode = p
+        if (p.val < key) {
+            p = p.right
+            isright = true
+        } else {
+            p = p.left
+            isright = false
+        }
+    }
+
+    if (p === null) return root;
+
+    //一个子节点都没有
+    if (p.left === null && p.right === null) {
+        if (parentNode === null) {
+            return null
+        }
+        if (isright) {
+            parentNode.right = null
+        } else {
+            parentNode.left = null 
+        }
+    }
+    
+    //有一个子节点
+    if (p.left === null && p.right !== null) {
+        if (parentNode === null) {
+            return p.right
+        }
+        if (isright) {
+            parentNode.right = p.right
+        } else {
+            parentNode.left = p.right 
+        }
+    }
+    if (p.left !== null && p.right === null) {
+        if (parentNode === null) {
+            return p.left
+        }
+        if (isright) {
+            parentNode.right = p.left
+        } else {
+            parentNode.left = p.left 
+        }
+    }
+    //有两个字节，找到右边树最小的（左边最后一个没有子节点的）
+    if (p.left !== null && p.right !== null) {
+        let childP = p.right
+        let parentChildP = p;
+        while (childP.left !== null) {
+            parentChildP = childP
+            childP = childP.left
+        }
+        if (childP === p.right) {
+            childP.left = p.left
+        } else {
+            parentChildP.left = null
+            if (childP.right !== null) {
+                parentChildP.left = childP.right
+                childP.right = null
+            }
+            childP.left = p.left
+            childP.right = p.right
+        }
+        //判断是否是删除的根节点
+        if (parentNode === null) {
+            return childP
+        }
+        if (isright) {
+            parentNode.right = childP
+        } else {
+            parentNode.left = childP 
+        }
+    }
+    return root
+};
+
+// deleteNode (node,50)
+
+//无重复字符的最长子串
+var lengthOfLongestSubstring = function(s) {
+    if (s.length <= 1) return s.length
+    let end = s.length
+    let result = 0;
+    for (let j = 0; j < end; j++) {
+        let childString = s[j]
+        for (let begin = j + 1; begin < end; begin++) {
+            let equl = false
+            for (let i = 0; i < childString.length; i++) {
+                if (childString[i] === s[begin]) {
+                    equl = true
+                    break
+                }
+            }
+            if (equl) {
+                if (childString.length > result) {
+                    result = childString.length
+                }
+                childString = ""
+                break
+            } else {
+                childString = childString + s[begin]
+                if (childString.length > result) {
+                    result = childString.length
+                }
+            }
+        }
+    }
+    return result
+};
+
+var lengthOfLongestSubstring2 = function (s) {
+    const set = new Set(); //判断滑动窗口内是否有重复元素
+    let i = 0,//滑动窗口右边界
+        j = 0,//滑动窗口左边界
+        maxLength = 0;
+    if (s.length === 0) {//极端情况
+        return 0;
+    }
+    for (i; i < s.length; i++) {
+        if (!set.has(s[i])) {//当前元素不在set中 就加入set 然后更新最大长度，i++继续下一轮循环
+            set.add(s[i]);
+            maxLength = Math.max(maxLength, set.size);
+        } else {
+            //set中有重复元素不断让j++ 并删除窗口之外的元素 直到滑动窗口内没有重复的元素
+            while (set.has(s[i])) {
+                set.delete(s[j]);
+                j++;
+            }
+            set.add(s[i]);//放心将s[i]加入set中
+        }
+    }
+    return maxLength;
+};
+
+console.log(lengthOfLongestSubstring2("umvejmcmuuk"))
